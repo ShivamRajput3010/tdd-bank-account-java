5e7ea15558382acca03ffc51e3575dd11abb53a4
Your Name <shivam.rajput3010@gmail.com>
2020-08-06 15:48:21 +0530

    balance and deposit check

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -10,6 +10,20 @@ public class AccountTest {
     @Test
 	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
+
         assertThat("your first test").isBlank();
+    }
+
+    @Test
+    public void checkZeroBalance() {
+        Account ac = new Account();
+        assertThat(ac.Balance()).isEqualTo(0);
+    }
+
+    @Test
+    public void depositAmountShouldIncreaseBalance(){
+        Account ac = new Account();
+        ac.deposit(10);
+        assertThat(ac.Balance()).isEqualTo(10);
     }
 }
